652fcd2a51b0a092990dccd6e6bd861826082f91
ZackSid07 <algalib908@gmail.com>
2025-03-04 14:33:11 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,19 @@
+var firstname = "galib";
+var age = 67;
+let lastname = "Al"
+console.log(firstname);
+console.log(age);
+console.log(lastname);
+
+//Naming a variable
+
+//camel casing 
+//e.g. : firstName {mltiple words but in word the s}
+
+
+
+
+
+
+
+
