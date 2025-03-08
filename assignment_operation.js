10a796f89b8f1e7ec46dca5b18db53d405fbaad3
ZackSid07 <algalib908@gmail.com>
2025-03-09 01:58:12 +0600

    new

---
+++ assignment_operation.js
@@ -0,0 +1,17 @@
+let num1 = 10, num2 = 20;
+
+
+num1 = num1 + num2
+
+nu1 += num2
+
+num1 *= num2
+
+num1 -= num2
+
+num1 %= num2
+
+num1 /= num2
+
+console.log(num1);
+
---
+++ comarission_operator.js
@@ -0,0 +1,6 @@
+let x = 5 , num = '10'
+
+console.log(x === num);
+
+console.log(x !== num);
+
